d6f61d5b4af588bee407ba59b0b8b44e8d966a87
Abdullah <abdullahchattha@gmail.com>
2020-02-13 17:43:39 -0500

    addition to main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,8 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         //System.out.println("Hello World");
-        //APriori test = new APriori("retail.txt");
-        //test.secondPass();
+        APriori test = new APriori("retail.txt");
+        test.secondPass();
         PCY test1 = new PCY("retail.txt");
         test1.secondPass();
     }
